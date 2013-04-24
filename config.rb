fa649296f303b62bc3ad287870fa8b9a17503271
Carwin Young <carwinyoung@gmail.com>
2013-04-24 15:10:16 -0500

    Issue #5: Remove duplicate code and switch to performance configuration.

--- config.rb
+++ config.rb
@@ -1,54 +1,33 @@
-# This file is only needed for Compass/Sass integration. If you are not using
-# Compass, you may safely ignore or delete this file.
+# This file sets up Sass/Compass to compile in production mode. Local development
+# environments should override these settings when required.
+#
+# Be mindful not to commit development changes back upstream.
 
-# Change this to :production when ready to deploy the CSS to the live server.
-  environment = :development
 
-# In development, we can turn on the FireSass-compatible debug_info.
-# firesass = false
-# comment above line and uncomment the next three lines to use firesass
-  firesass = true
-  Sass::Plugin.options[:debug_info] = true
-  sass_options = {:debug_info => true}
-#/ end firesass
+# Explicitly define the environment
+environment = :production
+
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# Disable debugging comments that display the location of your selectors.
+line_comments = false
 
 # Location of the theme's resources.
-  css_dir         = "css"
-  sass_dir        = "scss"
-  extensions_dir  = "../../libraries/sass-extensions"
-  images_dir      = "images"
-  javascripts_dir = "js"
-  fonts_dir       = "fonts"
+relative_assets = true                                     # Use relative paths.
+css_dir         = "css"
+sass_dir        = "scss"
+extensions_dir  = "../../libraries/sass-extensions"
+images_dir      = "images"
+javascripts_dir = "js"
+fonts_dir       = "fonts"
 
-# Require any additional compass plugin   s installed on your system.
-# require 'zen-grids'
-# require 'susy'
+# Silence deprecation warnings
+disable-warnings = true
 
-# Assuming this theme is in sites/*/themes/THEMENAME, you can add the partials
-# included with a module by uncommenting and modifying one of the lines below:
-# add_import_path "../../../default/modules/FOO"
-# add_import_path "../../../all/modules/FOO"
-# add_import_path "../../../../modules/FOO"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-  output_style = (environment == :development) ? :expanded : :compressed
-
-# To enable relative paths to assets via compass helper functions. Since Drupal
-# themes can be installed in multiple locations, we don't need to worry about
-# the absolute path to the theme from the server root.
-  relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-# Pass options to sass. For development, we turn on the FireSass-compatible
-# debug_info if the firesass config variable above is true.
-  sass_options = (environment == :development && firesass == true) ? {
-    :debug_info => true,
-    :unix_newlines => true
-  } : {
-    :quiet => true,
-    :unix_newlines => true
-  }
+# Pass production options to Sass and force Unix newlines just in case.
+sass_options = {
+  :quiet => true,
+  :unix_newlines => true
+}
 
