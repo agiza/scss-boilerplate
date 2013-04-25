6d6dc0236176f4951a3bd1e40f535236ccb04e9b
Kris Bulman <kris@bulman.ca>
2013-04-24 22:44:05 -0300

    Fixed config.rb compile problems  and set it into production mode Issue #7

--- config.rb
+++ config.rb
@@ -1,33 +1,35 @@
-# This file sets up Sass/Compass to compile in production mode. Local development
-# environments should override these settings when required.
-#
-# Be mindful not to commit development changes back upstream.
+# Change this to :production when ready to deploy the CSS to the live server.
+  environment = :production
 
-
-# Explicitly define the environment
-environment = :production
-
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
-
-# Disable debugging comments that display the location of your selectors.
-line_comments = false
+# In development, we can turn on the FireSass-compatible debug_info.
+  firesass = false
 
 # Location of the theme's resources.
-relative_assets = true                                     # Use relative paths.
-css_dir         = "css"
-sass_dir        = "scss"
-extensions_dir  = "../../libraries/sass-extensions"
-images_dir      = "images"
-javascripts_dir = "js"
-fonts_dir       = "fonts"
+  css_dir         = "css"
+  sass_dir        = "scss"
+  extensions_dir  = "../../libraries/sass-extensions"
+  images_dir      = "images"
+  javascripts_dir = "js"
+  fonts_dir       = "assets"
+  relative_assets = true
 
-# Silence deprecation warnings
-disable-warnings = true
+# Require any additional compass plugins installed on your system.
+# require 'zen-grids'
+# require 'susy'
 
-# Pass production options to Sass and force Unix newlines just in case.
-sass_options = {
-  :quiet => true,
-  :unix_newlines => true
-}
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+  output_style = (environment == :development) ? :expanded : :compressed
 
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+  line_comments = false
+
+# Pass options to sass. For development, we turn on the FireSass-compatible
+# debug_info if the firesass config variable above is true.
+  sass_options = (environment == :development && firesass == true) ? {
+    :debug_info => true,
+    :unix_newlines => true
+  } : {
+    :quiet => true,
+    :unix_newlines => true
+  }
